2c241b015b4d1e65fd095720aad33e13fa97346e
Lochlann O Neill <lochlannjoneill@gmail.com>
2024-02-22 04:48:33 +0000

    Browser Routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,34 @@
 import './App.css';
 import { Navbar } from './Components/Navbar/Navbar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Shop from './Pages/Shop';
+import ShopCategory from './Pages/ShopCategory';
+import Product from './Pages/Product';
+import Cart from './Pages/Cart';
+import LoginSignup from './Pages/LoginSignup';
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <BrowserRouter>
+        <Navbar/>
+        <Routes>
+          {/* <Route path={["/", "/shop"]} element={<Shop />} />
+          <Route path="/shop/:category" element={<ShopCategory />} />
+          <Route path="/shop/:category/:product" element={<Product />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login-signup" element={<LoginSignup />} /> */}
+          <Route path="/" element={<Shop />} />
+          <Route path="/men" element={<ShopCategory category="men"/>} />
+          <Route path="/women" element={<ShopCategory category="women"/>} />
+          <Route path="/kids" element={<ShopCategory category="kids"/>} />
+          <Route path="/product" element={<Product />}>
+            <Route path=":productId" element={<Product />} />
+          </Route>
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login-signup" element={<LoginSignup />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
