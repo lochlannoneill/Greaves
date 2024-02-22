9db23fdc6a9f2fd64175c171e3d66734c9026186
Lochlann O Neill <lochlannjoneill@gmail.com>
2024-02-22 08:18:31 +0000

    create divider

---
+++ frontend/src/Components/Divider/Divider.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Divider.css'
+
+export const Divider = () => {
+  return (
+    <div className="divider">
+        <h2>THE SPRING SALE IS HERE!</h2>
+        <p>Experience lower prices on all of our products.</p>
+    </div>
+  )
+}
--- frontend/src/Pages/Shop.jsx
+++ frontend/src/Pages/Shop.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Hero } from '../Components/Hero/Hero'
+import { Divider } from '../Components/Divider/Divider'
 
 export const Shop = () => {
   return (
     <div>
       <Hero/>
+      <Divider/>
     </div>
   )
 }
