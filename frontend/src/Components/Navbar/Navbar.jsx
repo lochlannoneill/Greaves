90bbbd488c26746eae99fcd27d8e3524ce3fba90
Lochlann O Neill <lochlannjoneill@gmail.com>
2024-02-22 03:45:25 +0000

    minor - navbar styling

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import './Navbar.css'
 import logo from '../Assets/logo192.png'
+import icon_search from '../Assets/icon_search.png'
 import icon_user  from '../Assets/icon_user.png'
 import icon_cart from '../Assets/icon_cart.png'
 
@@ -11,7 +12,7 @@ export const Navbar = () => {
     <div className='navbar'>
         <div className='nav-logo'>
             <img src={logo} alt='logo' />
-            <p>GREAVES</p>
+            <p>Greaves</p>
         </div>
         <ul className='nav-menu'>
             <li onClick={()=>{setMenu("shop")}}>Shop{menu==="shop"?<hr/>:<></>}</li>
@@ -20,6 +21,7 @@ export const Navbar = () => {
             <li onClick={()=>{setMenu("kids")}}>Kids{menu==="kids"?<hr/>:<></>}</li>
         </ul>
         <div className='nav-login-cart'>
+            <img className='nav-search' src={icon_search} alt='search' />
             <img className='nav-login' src={icon_user} alt='user' />
             <img className='nav-cart' src={icon_cart} alt='cart' />
             <div className='nav-cart-count'>0</div>
