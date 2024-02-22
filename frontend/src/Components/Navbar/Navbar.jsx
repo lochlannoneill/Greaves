19e59187142a3637290cf1c6c629a2073f263627
Lochlann O Neill <lochlannjoneill@gmail.com>
2024-02-22 03:26:43 +0000

    Create primitive navbar

---
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+import './Navbar.css'
+import logo from '../Assets/logo192.png'
+import icon_user  from '../Assets/icon_user.png'
+import icon_cart from '../Assets/icon_cart.png'
+
+export const Navbar = () => {
+    const [menu,setMenu] = useState("shop")
+
+  return (
+    <div className='navbar'>
+        <div className='nav-logo'>
+            <img src={logo} alt='logo' />
+            <p>GREAVES</p>
+        </div>
+        <ul className='nav-menu'>
+            <li onClick={()=>{setMenu("shop")}}>Shop{menu==="shop"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("men")}}>Men{menu==="men"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("women")}}>Women{menu==="women"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("kids")}}>Kids{menu==="kids"?<hr/>:<></>}</li>
+        </ul>
+        <div className='nav-login-cart'>
+            <img className='nav-login' src={icon_user} alt='user' />
+            <img className='nav-cart' src={icon_cart} alt='cart' />
+            <div className='nav-cart-count'>0</div>
+        </div>
+    </div>
+  )
+}
