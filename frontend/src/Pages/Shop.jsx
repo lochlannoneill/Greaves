178066493d48cf7e2899d50c2ececa38080102d2
Lochlann O Neill <lochlannjoneill@gmail.com>
2024-02-23 05:52:42 +0000

    add offers section

--- frontend/src/Pages/Shop.jsx
+++ frontend/src/Pages/Shop.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Hero } from '../Components/Hero/Hero'
 import { Divider } from '../Components/Divider/Divider'
 import { Popular } from '../Components/Popular/Popular'
+import { Offers } from '../Components/Offers/Offers'
 
 export const Shop = () => {
   return (
@@ -9,6 +10,7 @@ export const Shop = () => {
       <Hero/>
       <Divider/>
       <Popular/>
+      <Offers/>
     </div>
   )
 }
