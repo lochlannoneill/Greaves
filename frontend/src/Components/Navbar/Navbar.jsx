a105b9bd3d3722507d3d098a8afe718467fb003b
Lochlann O Neill <lochlannjoneill@gmail.com>
2024-02-23 00:34:56 +0000

    mobile dynamic navbar

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -10,23 +10,35 @@ export const Navbar = () => {
     const [menu,setMenu] = useState("shop")
 
   return (
-    <div className='navbar'>
-        <div className='nav-logo'>
-            {/* <img src={logo} alt='logo' /> */}
-            <p onClick={()=>{setMenu("shop")}}><Link to="/" style={{ textDecoration: 'none', color: '#171717'}}>Greaves</Link></p>
+    <div className='navbar-parent'>
+        <div className='navbar'>
+            <div className='nav-logo'>
+                {/* <img src={logo} alt='logo' /> */}
+                <p onClick={()=>{setMenu("shop")}}><Link to="/" style={{ textDecoration: 'none', color: '#171717'}}>Greaves</Link></p>
+            </div>
+            <ul className='nav-menu nav-menu-large'>
+                {/* <li onClick={()=>{setMenu("shop")}}><Link to="/" style={{ textDecoration: 'none', color: '#171717'}}>Shop</Link>{menu==="shop"?<hr/>:<></>}</li> */}
+                <li onClick={()=>{setMenu("women")}}><Link to="/women" style={{ textDecoration: 'none', color: '#171717'}}>Women</Link>{menu==="women"?<hr/>:<></>}</li>
+                <li onClick={()=>{setMenu("men")}}><Link to="/men" style={{ textDecoration: 'none', color: '#171717'}}>Men</Link>{menu==="men"?<hr/>:<></>}</li>
+                <li onClick={()=>{setMenu("girls")}}><Link to="/girls" style={{ textDecoration: 'none', color: '#171717'}}>Girls</Link>{menu==="girls"?<hr/>:<></>}</li>
+                <li onClick={()=>{setMenu("boys")}}><Link to="/boys" style={{ textDecoration: 'none', color: '#171717'}}>Boys</Link>{menu==="boys"?<hr/>:<></>}</li>
+            </ul>
+            <div className='nav-login-cart'>
+                <img className='nav-search' src={icon_search} alt='search' />
+                <Link to="/login-signup" style={{ textDecoration: 'none'}}><img className='nav-login' src={icon_user} alt='user' /></Link>
+                <Link to="/hearts" style={{ textDecoration: 'none'}}><img className='nav-cart' src={icon_heart_solid} alt='heart' /></Link>
+                <Link to="/cart" style={{ textDecoration: 'none'}}><img className='nav-cart' src={icon_cart} alt='cart' /></Link>
+                <div className='nav-cart-count'>0</div>
+            </div>
         </div>
-        <ul className='nav-menu'>
-            {/* <li onClick={()=>{setMenu("shop")}}><Link to="/" style={{ textDecoration: 'none', color: '#171717'}}>Shop</Link>{menu==="shop"?<hr/>:<></>}</li> */}
-            <li onClick={()=>{setMenu("men")}}><Link to="/men" style={{ textDecoration: 'none', color: '#171717'}}>Men</Link>{menu==="men"?<hr/>:<></>}</li>
-            <li onClick={()=>{setMenu("women")}}><Link to="/women" style={{ textDecoration: 'none', color: '#171717'}}>Women</Link>{menu==="women"?<hr/>:<></>}</li>
-            <li onClick={()=>{setMenu("kids")}}><Link to="/kids" style={{ textDecoration: 'none', color: '#171717'}}>Kids</Link>{menu==="kids"?<hr/>:<></>}</li>
-        </ul>
-        <div className='nav-login-cart'>
-            <img className='nav-search' src={icon_search} alt='search' />
-            <Link to="/login-signup" style={{ textDecoration: 'none'}}><img className='nav-login' src={icon_user} alt='user' /></Link>
-            <Link to="/hearts" style={{ textDecoration: 'none'}}><img className='nav-cart' src={icon_heart_solid} alt='heart' /></Link>
-            <Link to="/cart" style={{ textDecoration: 'none'}}><img className='nav-cart' src={icon_cart} alt='cart' /></Link>
-            <div className='nav-cart-count'>0</div>
+        <div className="navbar navbar-mobile">
+            <ul className='nav-menu'>
+                {/* <li onClick={()=>{setMenu("shop")}}><Link to="/" style={{ textDecoration: 'none', color: '#171717'}}>Shop</Link>{menu==="shop"?<hr/>:<></>}</li> */}
+                <li onClick={()=>{setMenu("women")}}><Link to="/women" style={{ textDecoration: 'none', color: '#171717'}}>Women</Link>{menu==="women"?<hr/>:<></>}</li>
+                <li onClick={()=>{setMenu("men")}}><Link to="/men" style={{ textDecoration: 'none', color: '#171717'}}>Men</Link>{menu==="men"?<hr/>:<></>}</li>
+                <li onClick={()=>{setMenu("girls")}}><Link to="/girls" style={{ textDecoration: 'none', color: '#171717'}}>Girls</Link>{menu==="girls"?<hr/>:<></>}</li>
+                <li onClick={()=>{setMenu("boys")}}><Link to="/boys" style={{ textDecoration: 'none', color: '#171717'}}>Boys</Link>{menu==="boys"?<hr/>:<></>}</li>
+            </ul>
         </div>
     </div>
   )
