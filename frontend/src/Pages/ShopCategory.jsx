ea9fccebb6e495762354e6eeee4ca067526e853a
Lochlann O Neill <lochlannjoneill@gmail.com>
2024-02-24 07:31:49 +0000

    add searchbar

--- frontend/src/Pages/ShopCategory.jsx
+++ frontend/src/Pages/ShopCategory.jsx
@@ -4,17 +4,34 @@ import { ShopContext } from '../Context/ShopContext'
 import { Item } from '../Components/Item/Item'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChevronDown } from '@fortawesome/free-solid-svg-icons'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
 
 export const ShopCategory = (props) => {
   const {products} = useContext(ShopContext);
   return (
     <div className="shopcategory">
       {/* <img className="shopcategory-banner" src={props.banner} alt="banner"/> */}
-      <div className="shopcatergory-sort">
-        <div className="shopcategory-sort-button">
+      <div className="shopcategory-filter">
+        <div className="shopcategory-filter-button">
           Sort by <FontAwesomeIcon icon={faChevronDown} size="2xs" />
         </div>
+        <div className="shopcategory-filter-search">
+          <form className="schopcatergory-filter-search-form">
+            <div className="shopcategory-filter-search-bar">
+              <div className="shopcategory-filter-search-icon">
+                <FontAwesomeIcon icon={faSearch} />
+              </div>
+              <input
+                className="shopcategory-filter-search-input"
+                type="text"
+                placeholder="Search products"
+              />
+            </div>
+            {/* <button type="shopcatergory-filter-search-button">Search</button> */}
+          </form>
+        </div>
       </div>
+      <p className="shopcategory-query">Results for '<b>placeholder</b>'</p>
       <div className="shopcategory-products-parent">
         <div className="shopcategory-products">
           {products.map((item,index)=>{
