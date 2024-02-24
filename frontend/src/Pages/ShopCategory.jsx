c30ff9f691161e35860ab6d69ef70a1cf38b983b
Lochlann O Neill <lochlannjoneill@gmail.com>
2024-02-24 06:32:40 +0000

    add content to ShopCategory

--- frontend/src/Pages/ShopCategory.jsx
+++ frontend/src/Pages/ShopCategory.jsx
@@ -1,12 +1,40 @@
 import React, { useContext } from 'react'
 import './CSS/ShopCategory.css'
 import { ShopContext } from '../Context/ShopContext'
+import { Item } from '../Components/Item/Item'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons'
 
 export const ShopCategory = (props) => {
-  const {all_products} = useContext(ShopContext);
+  const {products} = useContext(ShopContext);
   return (
-    <div className="shop-category">
-      <img src={props.banner} alt="banner" className="shop-category-banner"/>
+    <div className="shopcategory">
+      {/* <img className="shopcategory-banner" src={props.banner} alt="banner"/> */}
+      <div className="shopcatergory-sort">
+        <div className="shopcategory-sort-button">
+          Sort by <FontAwesomeIcon icon={faChevronDown} size="2xs" />
+        </div>
+      </div>
+      <div className="shopcategory-products-parent">
+        <div className="shopcategory-products">
+          {products.map((item,index)=>{
+            if (props.category===item.category) {
+              return <Item key={index} id={item.id} title={item.title} img={item.image} price={item.price} price_old={item.price_old} />
+            }
+            else {
+              return null;
+            }
+          })}
+        </div>
+        <div className="shopcategory-loadmore">
+          <p className="shopcategory-loadmore-results">
+            Showing <span>1-12</span> of 36 results
+          </p>
+          <div className="shopcategory-loadmore-button">
+            See More
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
