814a6709aea7a10612610c6887a1d4d0b06480e2
Lochlann O Neill <lochlannjoneill@gmail.com>
2024-02-24 03:00:41 +0000

    collections container div

--- frontend/src/Components/NewCollections/NewCollections.jsx
+++ frontend/src/Components/NewCollections/NewCollections.jsx
@@ -7,10 +7,12 @@ export const NewCollections = () => {
   return (
     <div className="new-collections">
         <h1>New Collections</h1>
-        <div className="collections">
-            {new_collection.map((item,index)=>{
-                return <Item key={index} id={item.id} title={item.title} img={item.image} price={item.price} price_old={item.price_old} />
-            })}
+        <div className="collections-parent">
+            <div className="collections">
+                {new_collection.map((item,index)=>{
+                    return <Item key={index} id={item.id} title={item.title} img={item.image} price={item.price} price_old={item.price_old} />
+                })}
+            </div>
         </div>
     </div>
   )
