b341ed2c6a9183639f215b6a92c3fc01fd525264
Lochlann O Neill <lochlannjoneill@gmail.com>
2024-03-10 14:12:01 +0000

    additions to footer + styling

--- frontend/src/Components/Footer/Footer.jsx
+++ frontend/src/Components/Footer/Footer.jsx
@@ -15,30 +15,42 @@ export const Footer = () => {
         <hr/>
         <div className="footer-information">
           <div className="footer-information-child footer-left">
-            <h2>GREAVES</h2>
-            <p>Copyright @ 2024 - All rights reserved</p>
-          </div>
-          <div className="footer-information-child footer-middle">
-            <p>Lorem</p>
+            <h2>Lorem</h2>
             <p>Ipsum</p>
             <p>Dolor</p>
             <p>Sit</p>
             <p>Amet</p>
           </div>
-          <div className="footer-information-child footer-right">
+          <div className="footer-information-child footer-middle">
             <h2>Show Us Your Haul</h2>
             <p>Tag us <a href="https://greaves.store/">#greaves</a> for a chance to be featured!</p>
-            <span className="footer-social"><a href="https://greaves.store/"><FontAwesomeIcon className="footer-icon instagram" icon={faInstagram} /></a></span>
-            <span className="footer-social"><a href="https://greaves.store/"><FontAwesomeIcon className="footer-icon facebook" icon={faFacebook} /></a></span>
-            <span className="footer-social"><a href="https://greaves.store/"><FontAwesomeIcon className="footer-icon twitter" icon={faTwitter} /></a></span>
-            <span className="footer-social"><a href="https://greaves.store/"><FontAwesomeIcon className="footer-icon snapchat" icon={faSnapchat} /></a></span>
-            <span className="footer-social"><a href="https://greaves.store/"><FontAwesomeIcon className="footer-icon tiktok" icon={faTiktok} /></a></span>
+            <div className="footer-social-icons">
+              <span className="footer-social"><a href="https://greaves.store/"><FontAwesomeIcon className="footer-icon instagram" icon={faInstagram} /></a></span>
+              <span className="footer-social"><a href="https://greaves.store/"><FontAwesomeIcon className="footer-icon facebook" icon={faFacebook} /></a></span>
+              <span className="footer-social"><a href="https://greaves.store/"><FontAwesomeIcon className="footer-icon twitter" icon={faTwitter} /></a></span>
+              <span className="footer-social"><a href="https://greaves.store/"><FontAwesomeIcon className="footer-icon snapchat" icon={faSnapchat} /></a></span>
+              <span className="footer-social"><a href="https://greaves.store/"><FontAwesomeIcon className="footer-icon tiktok" icon={faTiktok} /></a></span>
+            </div>
+          </div>
+          <div className="footer-information-child footer-right">
+          <h2>GREAVES</h2>
+            <p>&copy; 2024 Greaves. All Rights Reserved.</p>
+            <br />
+            <p>Call us @ 123-4567890</p>
+            <br />
+            <p>1234 Greaves Street,</p>
+            <p>Greaves, GR 12345</p>
+            <p>Imaginary County</p>
+            <p>Fantasyland</p>
           </div>
         </div>
-      </div>
+        </div>
       <hr/>
-      <p>Find the source code to this website in my <a href="https://github.com/lochlannoneill/greaves">GitHub repository</a>.</p>
-      <p className="signature"><a href="https://lochlannoneill.com/">Lochlann O Neill</a> • 2024</p>
+      <div className="footer-signature">
+        <p>Greaves is a fictional brand created by <a href="https://lochlannoneill.com/">Lochlann O Neill</a> for educational purposes only.</p>
+        <p>Find the source code to this website in my <a href="https://github.com/lochlannoneill/greaves">GitHub repository</a>.</p>
+        <p><a href="https://lochlannoneill.com/">Lochlann O Neill</a> • 2024</p>
+      </div>
     </footer>
   )
 }
