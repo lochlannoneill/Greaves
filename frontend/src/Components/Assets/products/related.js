3e95e3ef49af7836bf5a6085b0583f6ba7f89187
Lochlann O Neill <lochlannjoneill@gmail.com>
2024-03-14 22:24:03 +0000

    remove redundant

--- frontend/src/Components/Assets/products/related.js
+++ frontend/src/Components/Assets/products/related.js
@@ -2,8 +2,6 @@ import p1_img from './product_1.png'
 import p2_img from './product_2.png'
 import p3_img from './product_3.png'
 import p4_img from './product_4.png'
-import p5_img from "./product_5.png";
-import p6_img from "./product_6.png";
 
 let data_product = [
   {
