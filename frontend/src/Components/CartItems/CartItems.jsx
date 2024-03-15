69ca611380392fd98b58a1e5f610f0a8e1694790
Lochlann O Neill <lochlannjoneill@gmail.com>
2024-03-15 16:40:29 +0000

    'Add' button in cart

--- frontend/src/Components/CartItems/CartItems.jsx
+++ frontend/src/Components/CartItems/CartItems.jsx
@@ -2,23 +2,23 @@ import React from 'react'
 import { useContext } from 'react'
 import { ShopContext } from '../../Context/ShopContext'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import { faPlus, faXmark } from '@fortawesome/free-solid-svg-icons';
 import './CartItems.css'
 
 export const CartItems = () => {
-  const {products, cart, removeCart} = useContext(ShopContext);
+  const {products, cart, addCart, removeCart} = useContext(ShopContext);
 
   // Check if there are any items in the cart
   const cartEmpty = Object.values(cart).every(quantity => quantity <= 0);
 
   return (
     <div className="cartitems">
-      <h2>Cart Items</h2>
+      <h2>My Cart</h2>
       <hr />
       {products.map((product) => {
         if (cart[product.id] > 0) {
           return (
-            <div className="classitems-item-list" key={product.id}>
+            <div className="cartitems-list" key={product.id}>
               <div className="cartitems-item">
                 <div className="cartitems-item-left">
                   <img className="cartitems-item-left-image" src={product.image} alt=""/>
@@ -28,9 +28,13 @@ export const CartItems = () => {
                   <div className="cartitems-item-right-cost">
                     <p className="cartitems-item-right-price">{product.price} x </p>
                     <button className="cartitems-item-quantity">{cart[product.id]}</button>
-                    <p className="cartitems-item-right-total"> = {product.price * cart[product.id]}</p>
+                    <p className="cartitems-item-right-total"> = &euro;{product.price * cart[product.id]}</p>
                   </div>
-                  <button className="cartitems-item-right-remove" onClick={() => { removeCart(product.id) }}>Remove <FontAwesomeIcon className="cartitems-remove-icon" icon={faXmark} /></button>
+                  <div className="cartitems-right-actions">
+                    <button className="cartitems-item-right-add" onClick={() => { addCart(product.id) }}>Add <FontAwesomeIcon icon={faPlus} /></button>
+                    <p>or</p>
+                    <button className="cartitems-item-right-remove" onClick={() => { removeCart(product.id) }}>Remove <FontAwesomeIcon icon={faXmark} /></button>
+                  </div>
                 </div>
               </div>
             </div>
