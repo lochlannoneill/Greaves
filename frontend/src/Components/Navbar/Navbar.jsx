c272a0042139b15704a09bf2bd7ee313db8ab355
Lochlann O Neill <lochlannjoneill@gmail.com>
2024-03-16 20:44:23 +0000

    add cart counter to navbar

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,47 +1,56 @@
-import React, { useState } from 'react'
+import React, { useState, useContext, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser as faUser_regular } from '@fortawesome/free-regular-svg-icons';
-import { faHeart as faHeart_solid , faCartShopping as faCartShopping_solid } from '@fortawesome/free-solid-svg-icons';
-import './Navbar.css'
-import flag_ireland from '../Assets/flags/ireland.png'
+import { faHeart as faHeart_solid, faCartShopping as faCartShopping_solid } from '@fortawesome/free-solid-svg-icons';
+import './Navbar.css';
+import flag_ireland from '../Assets/flags/ireland.png';
+import { ShopContext } from '../../Context/ShopContext';
 
 export const Navbar = () => {
-    const [menu,setMenu] = useState("shop")
+    const [menu, setMenu] = useState("shop");
+    const { cart, getCartCount } = useContext(ShopContext); // Access the ShopContext
 
-  return (
-    <div className='navbar-parent'>
-        <div className='navbar'>
-            <div className='nav-logo'>
-                {/* <img src={logo} alt='logo' /> */}
-                <p onClick={()=>{setMenu("shop")}}><Link to="/">Greaves</Link></p>
-            </div>
-            <ul className='nav-menu nav-menu-large'>
-                {/* <li onClick={()=>{setMenu("shop")}}><Link to="/" style={{ textDecoration: 'none', color: '#171717'}}>Shop</Link>{menu==="shop"?<hr/>:<></>}</li> */}
-                <li onClick={()=>{setMenu("women")}}><Link to="/women">Women</Link>{menu==="women"?<hr/>:<></>}</li>
-                <li onClick={()=>{setMenu("men")}}><Link to="/men">Men</Link>{menu==="men"?<hr/>:<></>}</li>
-                <li onClick={()=>{setMenu("girls")}}><Link to="/girls">Girls</Link>{menu==="girls"?<hr/>:<></>}</li>
-                <li onClick={()=>{setMenu("boys")}}><Link to="/boys">Boys</Link>{menu==="boys"?<hr/>:<></>}</li>
-            </ul>
-            <div className='nav-login-cart'>
-                <img className='nav-login-cart-child nav-flag' src={flag_ireland} alt='ireland' />
-                <Link className='nav-login-cart-child' to="/signup"><FontAwesomeIcon className="nav-login" icon={faUser_regular} /></Link>
-                <Link className='nav-login-cart-child' to="/favourites"><FontAwesomeIcon className="nav-favourite" icon={faHeart_solid} /></Link>
-                <div className='nav-login-cart-child'>
-                    <Link to="/cart"><FontAwesomeIcon className="nav-cart" icon={faCartShopping_solid} /></Link>
-                    <div className='nav-cart-count'>0</div>
+    // State for total cart items count
+    const [totalCartItems, setTotalCartItems] = useState(0);
+
+    // Update total cart items count when cart changes
+    useEffect(() => {
+        const count = getCartCount();
+        setTotalCartItems(count);
+    }, [cart, getCartCount]);
+
+    return (
+        <div className='navbar-parent'>
+            <div className='navbar'>
+                <div className='nav-logo'>
+                    {/* <img src={logo} alt='logo' /> */}
+                    <p onClick={() => { setMenu("shop") }}><Link to="/">Greaves</Link></p>
+                </div>
+                <ul className='nav-menu nav-menu-large'>
+                    <li onClick={() => { setMenu("women") }}><Link to="/women">Women</Link>{menu === "women" ? <hr /> : <></>}</li>
+                    <li onClick={() => { setMenu("men") }}><Link to="/men">Men</Link>{menu === "men" ? <hr /> : <></>}</li>
+                    <li onClick={() => { setMenu("girls") }}><Link to="/girls">Girls</Link>{menu === "girls" ? <hr /> : <></>}</li>
+                    <li onClick={() => { setMenu("boys") }}><Link to="/boys">Boys</Link>{menu === "boys" ? <hr /> : <></>}</li>
+                </ul>
+                <div className='nav-login-cart'>
+                    <img className='nav-login-cart-child nav-flag' src={flag_ireland} alt='ireland' />
+                    <Link className='nav-login-cart-child' to="/signup"><FontAwesomeIcon className="nav-login" icon={faUser_regular} /></Link>
+                    <Link className='nav-login-cart-child' to="/favourites"><FontAwesomeIcon className="nav-favourite" icon={faHeart_solid} /></Link>
+                    <div className='nav-login-cart-child'>
+                        <Link to="/cart"><FontAwesomeIcon className="nav-cart" icon={faCartShopping_solid} /></Link>
+                        <div className='nav-cart-count'>{totalCartItems}</div> {/* Display total cart items count */}
+                    </div>
                 </div>
             </div>
+            <div className="navbar navbar-mobile">
+                <ul className='nav-menu'>
+                    <li onClick={() => { setMenu("women") }}><Link to="/women">Women</Link>{menu === "women" ? <hr /> : <></>}</li>
+                    <li onClick={() => { setMenu("men") }}><Link to="/men">Men</Link>{menu === "men" ? <hr /> : <></>}</li>
+                    <li onClick={() => { setMenu("girls") }}><Link to="/girls">Girls</Link>{menu === "girls" ? <hr /> : <></>}</li>
+                    <li onClick={() => { setMenu("boys") }}><Link to="/boys">Boys</Link>{menu === "boys" ? <hr /> : <></>}</li>
+                </ul>
+            </div>
         </div>
-        <div className="navbar navbar-mobile">
-            <ul className='nav-menu'>
-                {/* <li onClick={()=>{setMenu("shop")}}><Link to="/">Shop</Link>{menu==="shop"?<hr/>:<></>}</li> */}
-                <li onClick={()=>{setMenu("women")}}><Link to="/women">Women</Link>{menu==="women"?<hr/>:<></>}</li>
-                <li onClick={()=>{setMenu("men")}}><Link to="/men">Men</Link>{menu==="men"?<hr/>:<></>}</li>
-                <li onClick={()=>{setMenu("girls")}}><Link to="/girls">Girls</Link>{menu==="girls"?<hr/>:<></>}</li>
-                <li onClick={()=>{setMenu("boys")}}><Link to="/boys">Boys</Link>{menu==="boys"?<hr/>:<></>}</li>
-            </ul>
-        </div>
-    </div>
-  )
-}
+    )
+};
