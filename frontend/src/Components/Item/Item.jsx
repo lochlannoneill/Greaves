59656a0a612c9ecb3890d840b3af348b8aa953f8
Lochlann O Neill <lochlannjoneill@gmail.com>
2024-03-17 00:43:15 +0000

    make container a link instead of just image

--- frontend/src/Components/Item/Item.jsx
+++ frontend/src/Components/Item/Item.jsx
@@ -15,28 +15,28 @@ export const Item = (props) => {
           <div className="item-image-container">
             <img className="item-image" src={props.img} alt={props.title} />
           </div>
-        </Link>
-        <div className="item-description">
-          <p>{props.title}</p>
-          <div className="item-stuff">
-            <div className="item-prices">
-                <div className="item-price-new">
-                  €{props.price}
-                </div>
-                <div className="item-price-old">
-                  €{props.price_old}
-                </div>
-            </div>
-            <div className="item-price-favourite">
-              {isFavorite(props.id) ? (
-                <FontAwesomeIcon
-                    className={`item-favourite ${isFavorite(props.id) ? 'isFavorite' : ''}`}
-                    icon={faHeart_solid}
-                />
-              ) : null}
+          <div className="item-description">
+            <p>{props.title}</p>
+            <div className="item-stuff">
+              <div className="item-prices">
+                  <div className="item-price-new">
+                    €{props.price}
+                  </div>
+                  <div className="item-price-old">
+                    €{props.price_old}
+                  </div>
+              </div>
+              <div className="item-price-favourite">
+                {isFavorite(props.id) ? (
+                  <FontAwesomeIcon
+                      className={`item-favourite ${isFavorite(props.id) ? 'isFavorite' : ''}`}
+                      icon={faHeart_solid}
+                  />
+                ) : null}
+              </div>
             </div>
           </div>
-        </div>
+      </Link>
     </div>
   )
 }
