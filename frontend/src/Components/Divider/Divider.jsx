6cc666c794553d38302839f0ea63d34850f1d320
Lochlann O Neill <lochlannjoneill@gmail.com>
2024-03-19 22:20:37 +0000

    return divider to inform about coupon codes

--- frontend/src/Components/Divider/Divider.jsx
+++ frontend/src/Components/Divider/Divider.jsx
@@ -4,8 +4,8 @@ import './Divider.css'
 export const Divider = () => {
   return (
     <div className="divider">
-      <h2>THE SPRING SALE IS HERE!</h2>
-      <p>Free shipping on all orders over $100. Orders placed now will ship on February 29th.</p>
+      <h2>THE SPRING SALE IS HERE</h2>
+      <p>Use code 'LOCHLANN' for -10% on all products!</p>
     </div>
   )
 }
