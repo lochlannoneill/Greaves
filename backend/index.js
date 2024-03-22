dca31cd34a0260f742df6afb43addbf462d93bcb
Lochlann O Neill <lochlannjoneill@gmail.com>
2024-03-22 19:32:36 +0000

    syntax

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,8 @@ app.use(cors());
 // MongoDB database connection
 mongoose
   .connect(
-    "mongodb+srv://lochlannoneill:zADROgfBCV2oUdx8@cluster0.5jlntnb.mongodb.net/greaves"
+    "mongodb+srv://lochlannoneill:zADROgfBCV2oUdx8@cluster0.5jlntnb.mongodb.net/greaves",
+    {}
   )
   .then(() => {
     console.log("MongoDB connected successfully.");
