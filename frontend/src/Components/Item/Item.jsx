8c60958e4fc77695cf7e3c096112c216a06e4465
Lochlann O Neill <lochlannjoneill@gmail.com>
2024-03-25 19:37:54 +0000

    Consolidate useContext declaration

--- frontend/src/Components/Item/Item.jsx
+++ frontend/src/Components/Item/Item.jsx
@@ -21,7 +21,8 @@ const truncateTitle = (title) => {
 };
 
 export const Item = (props) => {
-  const { isFavorite, isInCart, reviews, getReviewInfo } = useContext(ShopContext);
+  const { isFavorite, isInCart, reviews, getReviewInfo } =
+    useContext(ShopContext);
   const [reviewCount, setReviewCount] = useState(0);
   const [reviewAverageRating, setReviewAverageRating] = useState(0);
   console.log(`${props.id} + ${props.reviews}`);
@@ -36,42 +37,43 @@ export const Item = (props) => {
 
   return (
     <div className="item">
-      <Link to={`/products/${props.id}`} onClick={() => window.scrollTo(0, 0)}> 
+      <Link to={`/products/${props.id}`} onClick={() => window.scrollTo(0, 0)}>
         <div className="item-image-container">
           <img className="item-image" src={props.img} alt={props.title} />
         </div>
         <div className="item-description">
-          <p className="item-description-title">
-            {truncateTitle(props.title)}
-          </p>
-          <div className="item-description-reviews">
-            <p className="item-description-reviews-rating">{reviewAverageRating}</p>
-            <span className="item-description-reviews-stars">
-              {[...Array(Math.floor(reviewAverageRating))].map((_, index) => (
-                <FontAwesomeIcon
-                  key={index}
-                  className="item-description-reviews-star-icon"
-                  icon={faStar_solid}
-                />
-              ))}
-              {reviewAverageRating % 1 !== 0 && (
-                <FontAwesomeIcon
-                  className="item-description-reviews-star-icon"
-                  icon={faStar_half}
-                />
-              )}
-              {[...Array(Math.max(0, 5 - Math.ceil(reviewAverageRating)))].map(
-                (_, index) => (
+          <p className="item-description-title">{truncateTitle(props.title)}</p>
+          {reviewCount > 0 && (
+            <div className="item-description-reviews">
+              <span className="item-description-reviews-stars">
+                {[...Array(Math.floor(reviewAverageRating))].map((_, index) => (
+                  <FontAwesomeIcon
+                    key={index}
+                    className="item-description-reviews-star-icon"
+                    icon={faStar_solid}
+                  />
+                ))}
+                {reviewAverageRating % 1 !== 0 && (
+                  <FontAwesomeIcon
+                    className="item-description-reviews-star-icon"
+                    icon={faStar_half}
+                  />
+                )}
+                {[
+                  ...Array(Math.max(0, 5 - Math.ceil(reviewAverageRating))),
+                ].map((_, index) => (
                   <FontAwesomeIcon
                     key={index}
                     className="item-description-reviews-star-icon"
                     icon={faStar_regular}
                   />
-                )
-              )}
-            </span>
-            <p className="item-description-reviews-text">{reviewCount} reviews</p>
-          </div>
+                ))}
+              </span>
+              <p className="item-description-reviews-text">
+                {reviewCount} reviews
+              </p>
+            </div>
+          )}
           <div className="item-stuff">
             <div className="item-prices">
               <div className={`item-price ${props.price_old ? "reduced" : ""}`}>
