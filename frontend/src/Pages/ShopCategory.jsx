cdff5288c823d4d2077fba2fae7740887383a291
Lochlann O Neill <lochlannjoneill@gmail.com>
2024-03-25 18:58:46 +0000

    syntax

--- frontend/src/Pages/ShopCategory.jsx
+++ frontend/src/Pages/ShopCategory.jsx
@@ -1,5 +1,4 @@
-import React, { useContext } from "react";
-import { useEffect } from "react";
+import React, { useContext, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch, faChevronDown } from "@fortawesome/free-solid-svg-icons";
 import { ShopContext } from "../Context/ShopContext";
