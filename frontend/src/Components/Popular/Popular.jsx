ee818bf249cbb9899cc7c92a0d992a2e6ae6d097
Lochlann O Neill <lochlannjoneill@gmail.com>
2024-03-26 00:48:40 +0000

    Popular.jsx now uses product tags instead of its own database file

--- frontend/src/Components/Popular/Popular.jsx
+++ frontend/src/Components/Popular/Popular.jsx
@@ -1,7 +1,7 @@
+
 import React from "react";
-import products_women from "../../Assets/products/product_data_popular_women.js";
-import products_men from "../../Assets/products/product_data_popular_men.js";
-import { Item } from "../Item/Item";
+import products from "../../Assets/products/product_data.js";
+import { ItemList } from "../ItemList/ItemList";
 import "./Popular.css";
 
 export const Popular = () => {
@@ -10,40 +10,14 @@ export const Popular = () => {
       <div className="popular-category popular-women">
         <h1>Popular in Women</h1>
         <div className="popular-list">
-          <div className="popular-item">
-            {products_women.map((item, index) => {
-              return (
-                <Item
-                  key={index}
-                  id={item.id}
-                  title={item.title}
-                  img={item.image}
-                  price={item.price}
-                  price_previous={item.price_previous}
-                />
-              );
-            })}
-          </div>
+          <ItemList products={products} category="Women" />
         </div>
       </div>
 
       <div className="popular-category popular-men">
         <h1>Popular in Men</h1>
         <div className="popular-list">
-          <div className="popular-item">
-            {products_men.map((item, index) => {
-              return (
-                <Item
-                  key={index}
-                  id={item.id}
-                  title={item.title}
-                  img={item.image}
-                  price={item.price}
-                  price_previous={item.price_previous}
-                />
-              );
-            })}
-          </div>
+          <ItemList products={products} category="Men" />
         </div>
       </div>
     </div>
