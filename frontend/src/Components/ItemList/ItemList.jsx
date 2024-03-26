8d055e0ec570a336346ae83a791abe3c0133915e
Lochlann O Neill <lochlannjoneill@gmail.com>
2024-03-26 01:19:36 +0000

    remove the category parameter and associated filtering logic
    Now, the component renders all products passed to it without filtering by category.

--- frontend/src/Components/ItemList/ItemList.jsx
+++ frontend/src/Components/ItemList/ItemList.jsx
@@ -2,27 +2,20 @@ import React from "react";
 import { Item } from "../Item/Item";
 import "./ItemList.css";
 
-export const ItemList = ({ products, category }) => {
+export const ItemList = ({ products }) => {
   return (
     <div className="shopcategory-products">
-      {products.map((item, index) => {
-        // Check if category is defined before converting to lowercase
-        if (item.category?.toLowerCase() === category.toLowerCase()) {
-          return (
-            <Item
-              key={index}
-              id={item.id}
-              title={item.title}
-              img={item.image}
-              price={item.price}
-              price_previous={item.price_previous}
-              reviews={item.reviews}
-            />
-          );
-        } else {
-          return null;
-        }
-      })}
+      {products.map((item, index) => (
+        <Item
+          key={index}
+          id={item.id}
+          title={item.title}
+          img={item.image}
+          price={item.price}
+          price_previous={item.price_previous}
+          reviews={item.reviews}
+        />
+      ))}
     </div>
   );
 };
