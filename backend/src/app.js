8f9eb2e2c09a4c96d7ca004f35eb759d686c8662
Lochlann O Neill <lochlannjoneill@gmail.com>
2024-03-27 01:53:33 +0000

    update mongodb connection with .env export

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,4 @@
 const path = require("path");
-
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
@@ -12,17 +11,18 @@ app.use(express.json());
 app.use(cors());
 
 // MongoDB database connection
-mongoose.connect(
-  "mongodb+srv://lochlannoneill:zADROgfBCV2oUdx8@cluster0.5jlntnb.mongodb.net/greaves",
-  {}
-)
-.then(() => {
-  console.log("MongoDB connected: cluster0.5jlntnb.mongodb.net/greaves");
-})
-.catch((err) => {
-  console.error("Failed to connect to MongoDB:", err);
-  process.exit(1); // Exit process on MongoDB connection failure
-});
+mongoose
+  .connect(
+    "mongodb+srv://lochlannoneill:zADROgfBCV2oUdx8@cluster0.5jlntnb.mongodb.net/greaves",
+    {}
+  )
+  .then(() => {
+    console.log("MongoDB connected: cluster0.5jlntnb.mongodb.net/greaves");
+  })
+  .catch((err) => {
+    console.error("Failed to connect to MongoDB:", err);
+    process.exit(1); // Exit process on MongoDB connection failure
+  });
 
 // API Creation
 app.use("/", routes); // Just pass the router object
