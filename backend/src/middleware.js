2611ceafbe134afd13c94f087512d9bded44f790
Lochlann O Neill <lochlannjoneill@gmail.com>
2024-03-27 01:44:41 +0000

    Update backend architectural hierarchy

---
+++ backend/src/middleware.js
@@ -0,0 +1,19 @@
+const multer = require("multer");
+const path = require("path");
+
+// Image storage engine
+const storage = multer.diskStorage({
+  destination: path.join(__dirname, "upload/images"), // Absolute path for file upload
+  filename: function (req, file, cb) {
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  },
+});
+
+const upload = multer({
+  storage: storage,
+}).single("product");
+
+module.exports = upload;
---
+++ backend/src/models/productModel.js
@@ -0,0 +1,70 @@
+const mongoose = require("mongoose");
+
+// Schema for creating products
+const productSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  tags: [String],
+  categories: [String],
+  rating: {
+    type: Number,
+    default: 0,
+  },
+  reviews: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Review",
+    },
+  ],
+  stock: {
+    small: {
+      type: Number,
+      min: 0,
+      default: 0,
+    },
+    medium: {
+      type: Number,
+      min: 0,
+      default: 0,
+    },
+    large: {
+      type: Number,
+      min: 0,
+      default: 0,
+    },
+    xlarge: {
+      type: Number,
+      min: 0,
+      default: 0,
+    },
+    xxlarge: {
+      type: Number,
+      min: 0,
+      default: 0,
+    },
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  price_old: Number,
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// Creating a Mongoose model for the product schema
+const Product = mongoose.model("Product", productSchema);
+
+module.exports = Product;
