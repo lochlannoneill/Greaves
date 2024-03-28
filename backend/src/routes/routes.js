017d9afd2f7fb31bfa75279869ec0764ece3955c
Lochlann O Neill <lochlannjoneill@gmail.com>
2024-03-28 02:52:19 +0000

    User HTTP requests
    // post - new user
    // GET - all users
    // GET - user by ID
    // DELETE - user by ID
    // PUT - update user by ID

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const Product = require("../models/productModel");
+const User = require("../models/userModel");
+const Review = require("../models/reviewModel");
 const upload = require("../middleware");
 
 // Define routes
@@ -234,6 +236,115 @@ router.delete("/products/:id/reviews/:reviewId", (req, res) => {
           success: false,
           message: "Failed to delete review from product",
         });
+    });
+});
+
+// Post - new user
+router.post("/users", (req, res) => {
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+    firstname: req.body.firstname,
+    lastname: req.body.lastname,
+    image: req.body.image,
+    isVerified: req.body.isVerified,
+    isAdmin: req.body.isAdmin,
+  });
+  console.log("Adding user:", newUser);
+  newUser
+    .save()
+    .then(() => {
+      console.log("User added: " + req.body.username);
+      res.json({
+        success: true,
+        message: "User added: " + req.body.username,
+      });
+    })
+    .catch((err) => {
+      console.error("Failed to add user:", err);
+      res
+        .status(500)
+        .json({ success: false, message: "Failed to add user" });
+    });
+});
+
+// GET - all users
+router.get("/users", (req, res) => {
+  User.find()
+    .then((users) => {
+      console.log("Users retrieved.");
+      res.json(users);
+    })
+    .catch((err) => {
+      console.error("Failed to retrieve users:", err);
+      res
+        .status(500)
+        .json({ success: false, message: "Failed to retrieve users" });
+    });
+});
+
+// GET - user by ID
+router.get("/users/:id", (req, res) => {
+  const userId = req.params.id;
+  User.findById(userId)
+    .then((user) => {
+      console.log("User retrieved:", userId);
+      res.json(user);
+    })
+    .catch((err) => {
+      console.error("Failed to retrieve user:", err);
+      res
+        .status(500)
+        .json({ success: false, message: "Failed to retrieve user" });
+    });
+});
+
+// DELETE - user by ID
+router.delete("/users/:id", (req, res) => {
+  const userId = req.params.id;
+  User.findByIdAndDelete(userId)
+    .then(() => {
+      console.log("User deleted:", userId);
+      res.json({
+        success: true,
+        message: "User deleted",
+      });
+    })
+    .catch((err) => {
+      console.error("Failed to delete user:", err);
+      res
+        .status(500)
+        .json({ success: false, message: "Failed to delete user" });
+    });
+});
+
+// PUT - update user by ID
+router.put("/users/:id", (req, res) => {
+  const userId = req.params.id;
+  User.findByIdAndUpdate(
+    userId,
+    {
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+      firstname: req.body.firstname,
+      lastname: req.body.lastname,
+      image: req.body.image,
+      isVerified: req.body.isVerified,
+      isAdmin: req.body.isAdmin,
+    },
+    { new: true }
+  )
+    .then((user) => {
+      console.log("User updated:", userId);
+      res.json(user);
+    })
+    .catch((err) => {
+      console.error("Failed to update user:", err);
+      res
+        .status(500)
+        .json({ success: false, message: "Failed to update user" });
     });
 });
 
