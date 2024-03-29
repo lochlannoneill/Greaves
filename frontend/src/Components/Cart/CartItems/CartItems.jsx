60ad1bc88478ff943d830ba9a7c7ff0efe6bacdb
Lochlann O Neill <lochlannjoneill@gmail.com>
2024-03-29 04:37:02 +0000

    add favourted button to cart items

--- frontend/src/Components/Cart/CartItems/CartItems.jsx
+++ frontend/src/Components/Cart/CartItems/CartItems.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { useContext } from "react";
+import React, { useContext } from "react";
 import { ShopContext } from "../../../Context/ShopContext";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlus, faMinus, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { faPlus, faMinus, faTrash, faHeart as faHeart_solid } from "@fortawesome/free-solid-svg-icons";
+import { faHeart as faHeart_regular } from "@fortawesome/free-regular-svg-icons";
 import { Link } from "react-router-dom";
 import "./CartItems.css";
 
 export const CartItems = () => {
-  const { products, cart, addCart, removeCart, removeAllCart } =
-    useContext(ShopContext); // Added removeAllCart
+  const { products, cart, addCart, removeCart, removeAllCart, toggleFavorite, isFavorite } =
+    useContext(ShopContext); // Added toggleFavorite and isFavorite
   const cartEmpty = Object.values(cart).every((quantity) => quantity <= 0); // Check if there are any items in the cart
 
   return (
@@ -40,45 +42,68 @@ export const CartItems = () => {
                   <p className="cartitems-item-right-size">
                     Size: product.size
                   </p>
-                  <div className="cartitems-item-right-cost">
-                    <p className="cartitems-item-right-quantity">
-                      {cart[product.id]}
                     </p>
-                    <p>x</p>
-                    <p className="cartitems-item-price">
-                      &euro;{product.price}
                     </p>
-                    <p>=</p>
-                    <p className="cartitems-item-right-total">
-                      &euro;{(product.price * cart[product.id]).toFixed(2)}
                     </p>
                   </div>
-                  <div className="cartitems-right-actions">
-                    <button
-                      className="cartitems-item-right-add"
-                      onClick={() => {
-                        addCart(product.id);
-                      }}
-                    >
-                      <FontAwesomeIcon icon={faPlus} />
-                    </button>
-                    <button
-                      className="cartitems-item-right-remove"
-                      onClick={() => {
-                        removeCart(product.id);
-                      }}
-                    >
-                      <FontAwesomeIcon icon={faMinus} />
-                    </button>
-                    <button
-                      className="cartitems-item-right-delete"
-                      onClick={() => {
-                        removeAllCart(product.id);
-                      }}
-                    >
-                      <FontAwesomeIcon icon={faTrash} />
-                    </button>{" "}
-                    {/* Added Delete button */}
+                  <div className="cartitems-item-right-calculations">
+                    <div className="cartitems-item-right-cost">
+                      <p className="cartitems-item-right-quantity">
+                        {cart[product.id]}
+                      </p>
+                      <p>x</p>
+                      <p className="cartitems-item-price">
+                        &euro;{product.price}
+                      </p>
+                      <p>=</p>
+                      <p className="cartitems-item-right-total">
+                        &euro;{(product.price * cart[product.id]).toFixed(2)}
+                      </p>
+                    </div>
+                    <div className="cartitems-right-actions">
+                      <div className="cartiems-right-actions-left">
+                        <button
+                          className="cartitems-item-right-add"
+                          onClick={() => {
+                            addCart(product.id);
+                          }}
+                        >
+                          <FontAwesomeIcon icon={faPlus} />
+                        </button>
+                        <button
+                          className="cartitems-item-right-remove"
+                          onClick={() => {
+                            removeCart(product.id);
+                          }}
+                        >
+                          <FontAwesomeIcon icon={faMinus} />
+                        </button>
+                      </div>
+                      <div className="cartitems-right-actions-right">
+                        <button
+                          className="cartitems-item-right-delete"
+                          onClick={() => {
+                            removeAllCart(product.id);
+                          }}
+                        >
+                          <FontAwesomeIcon icon={faTrash} />
+                        </button>{" "}
+                        <button
+                          onClick={() => {
+                            toggleFavorite(product.id);
+                          }}
+                          className={`cartitems-item-right-favourite ${
+                            isFavorite(product.id) ? "in-favorites" : "not-in-favorites"
+                          }`}
+                        >
+                          {isFavorite(product.id) ? (
+                            <FontAwesomeIcon icon={faHeart_solid} />
+                          ) : (
+                            <FontAwesomeIcon icon={faHeart_regular} />
+                          )}
+                        </button>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
