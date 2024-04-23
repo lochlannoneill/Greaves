bd8905764e658652d2643272ab59bdf1ae525493
Lochlann O Neill <lochlannjoneill@gmail.com>
2024-04-23 09:06:02 +0100

    fix - Cloudflare linter

--- frontend/src/Pages/ShopCategory/ShopCategory.jsx
+++ frontend/src/Pages/ShopCategory/ShopCategory.jsx
@@ -2,7 +2,7 @@ import React, { useContext, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch, faChevronDown } from "@fortawesome/free-solid-svg-icons";
 import { ShopContext } from "../../Context/ShopContext";
-import { Filters } from "../../Components/Filters/Filters";
+// import { Filters } from "../../Components/Filters/Filters";
 import { ItemList } from "../../Components/ItemList/ItemList";
 import "./ShopCategory.css";
 
