13523160c941b4073a94123e35599c9ca22db7cc
Lochlann O Neill <lochlannjoneill@gmail.com>
2024-09-29 12:49:51 +0100

    increase image upload size limit to 5MB

--- backend/src/routes/image/middleware.js
+++ backend/src/routes/image/middleware.js
@@ -14,6 +14,7 @@ const storage = multer.diskStorage({
 
 const upload = multer({
   storage: storage,
+  limits: { fileSize: 1024 * 1024 * 5 }, // limit to 5MB
 }).single("image");
 
 module.exports = upload;
