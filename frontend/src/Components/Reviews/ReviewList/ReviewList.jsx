4f3120061e27500278dc7be81b78ab4e72fdab8e
Lochlann O Neill <lochlannjoneill@gmail.com>
2024-10-13 23:40:48 +0100

    create review average section

--- frontend/src/Components/Reviews/ReviewList/ReviewList.jsx
+++ frontend/src/Components/Reviews/ReviewList/ReviewList.jsx
@@ -47,23 +47,29 @@ export const ReviewList = ({ reviews }) => {
 
   return (
     <div className="reviewlist">
-      <div className="reviewlist-sort">
-        <label className="reviewlist-sort-label">Sort by:</label>
-        <div className="reviewlist-sort-filter">
-          <div className="reviewlist-sort-filter-option">
-            <select id="sortOption" value={sortOption} onChange={handleSortChange}>
-              <option value="rating">Highest Ratings</option>
-              <option value="lowestRating">Lowest Ratings</option> {/* Added option for lowest ratings */}
-              <option value="date">Most Recent</option>
-              <option value="helpful">Most Helpful</option>
-            </select>
-          </div>
+      <div className="reviewlist-header">
+        <div className="reviewlist-ratings">
+          <p>Based on {reviews.length} reviews</p>
+          <p>4.5 Average Rating</p>
         </div>
-        <div className="reviewlist-sort-verified">
-          <label className="reviewlist-sort-verified-label">
-            Verified
-            <input type="checkbox" checked={showVerifiedOnly} onChange={handleVerifiedChange} />
-          </label>
+        <div className="reviewlist-sort">
+          <label className="reviewlist-sort-label">Sort by:</label>
+          <div className="reviewlist-sort-filter">
+            <div className="reviewlist-sort-filter-option">
+              <select id="sortOption" value={sortOption} onChange={handleSortChange}>
+                <option value="rating">Highest Ratings</option>
+                <option value="lowestRating">Lowest Ratings</option> {/* Added option for lowest ratings */}
+                <option value="date">Most Recent</option>
+                <option value="helpful">Most Helpful</option>
+              </select>
+            </div>
+          </div>
+          <div className="reviewlist-sort-verified">
+            <label className="reviewlist-sort-verified-label">
+              Verified
+              <input type="checkbox" checked={showVerifiedOnly} onChange={handleVerifiedChange} />
+            </label>
+          </div>
         </div>
       </div>
       <div className="reviewlist-reviews">
