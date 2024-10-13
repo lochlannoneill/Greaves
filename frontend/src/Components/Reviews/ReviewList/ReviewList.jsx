2f33226181e5b452135e241b8950096dac0b63e4
Lochlann O Neill <lochlannjoneill@gmail.com>
2024-10-14 00:37:12 +0100

    display message if no reviews

--- frontend/src/Components/Reviews/ReviewList/ReviewList.jsx
+++ frontend/src/Components/Reviews/ReviewList/ReviewList.jsx
@@ -47,91 +47,95 @@ export const ReviewList = ({ reviews }) => {
 
   return (
     <div className="reviewlist">
-      <div className="reviewlist-group">
-        <div className="reviewlist-left">
-          <div className="reviewlist-average">
-            <h3>Average Ratings</h3>
-            <div className="reviewlist-average-rating">
-              <p>4.5 ★★★★☆ out of 100 reviews</p>
+      {reviews.length === 0 ? (
+        <div className="reviewlist-no-reviews">There are no reviews yet</div>
+      ) : (
+        <div className="reviewlist-group">
+          <div className="reviewlist-left">
+            <div className="reviewlist-average">
+              <h3>Average Ratings</h3>
+              <div className="reviewlist-average-rating">
+                <p>4.5 ★★★★☆ out of 100 reviews</p>
+              </div>
+              <div className="reviewlist-average-rating-breakdown">
+                <div className="reviewlist-average-rating-breakdown-item">
+                  <span>5</span>
+                  {/* TODO - THIS BAR ENDS SLIGHTLY BEFORE OTHERS */}
+                  <div className="reviewlist-average-rating-breakdown-bar">
+                    <div className="reviewlist-average-rating-breakdown-bar-fill" style={{ width: "80%" }} />
+                  </div>
+                  <span>80%</span>
+                </div>
+                <div className="reviewlist-average-rating-breakdown-item">
+                  <span>4</span>
+                  <div className="reviewlist-average-rating-breakdown-bar">
+                    <div className="reviewlist-average-rating-breakdown-bar-fill" style={{ width: "10%" }} />
+                  </div>
+                  <span>10%</span>
+                </div>
+                <div className="reviewlist-average-rating-breakdown-item">
+                  <span>3</span>
+                  <div className="reviewlist-average-rating-breakdown-bar">
+                    <div className="reviewlist-average-rating-breakdown-bar-fill" style={{ width: "5%" }} />
+                  </div>
+                  <span>5%</span>
+                </div>
+                <div className="reviewlist-average-rating-breakdown-item">
+                  <span>2</span>
+                  <div className="reviewlist-average-rating-breakdown-bar">
+                    <div className="reviewlist-average-rating-breakdown-bar-fill" style={{ width: "3%" }} />
+                  </div>
+                  <span>3%</span>
+                </div>
+                <div className="reviewlist-average-rating-breakdown-item">
+                  <span>1</span>
+                  {/* TODO - THIS BAR STARTS SLIGHTLY BEFORE OTHERS */}
+                  <div className="reviewlist-average-rating-breakdown-bar">
+                    <div className="reviewlist-average-rating-breakdown-bar-fill" style={{ width: "2%" }} />
+                  </div>
+                  <span>2%</span>
+                </div>
+              </div>
             </div>
-            <div className="reviewlist-average-rating-breakdown">
-              <div className="reviewlist-average-rating-breakdown-item">
-                <span>5</span>
-                {/* // TODO - THIS BAR ENDS SLIGHTLY BEFORE OTHERS */}
-                <div className="reviewlist-average-rating-breakdown-bar">
-                  <div className="reviewlist-average-rating-breakdown-bar-fill" style={{ width: "80%" }} />
-                </div>
-                <span>80%</span>
+          </div>
+          <div className="reviewlist-right">
+            <div className="reviewlist-header">
+              <div className="reviewlist-placeholder">
+                <h2>Customer Reviews</h2>
               </div>
-              <div className="reviewlist-average-rating-breakdown-item">
-                <span>4</span>
-                <div className="reviewlist-average-rating-breakdown-bar">
-                  <div className="reviewlist-average-rating-breakdown-bar-fill" style={{ width: "10%" }} />
+              <div className="reviewlist-sort">
+                <label className="reviewlist-sort-label">Sort by:</label>
+                <div className="reviewlist-sort-filter">
+                  <div className="reviewlist-sort-filter-option">
+                    <select id="sortOption" value={sortOption} onChange={handleSortChange}>
+                      <option value="rating">Highest Ratings</option>
+                      <option value="lowestRating">Lowest Ratings</option> {/* Added option for lowest ratings */}
+                      <option value="date">Most Recent</option>
+                      <option value="helpful">Most Helpful</option>
+                    </select>
+                  </div>
                 </div>
-                <span>10%</span>
-              </div>
-              <div className="reviewlist-average-rating-breakdown-item">
-                <span>3</span>
-                <div className="reviewlist-average-rating-breakdown-bar">
-                  <div className="reviewlist-average-rating-breakdown-bar-fill" style={{ width: "5%" }} />
+                <div className="reviewlist-sort-verified">
+                  <label className="reviewlist-sort-verified-label">
+                    Verified
+                    <input type="checkbox" checked={showVerifiedOnly} onChange={handleVerifiedChange} />
+                  </label>
                 </div>
-                <span>5%</span>
               </div>
-              <div className="reviewlist-average-rating-breakdown-item">
-                <span>2</span>
-                <div className="reviewlist-average-rating-breakdown-bar">
-                  <div className="reviewlist-average-rating-breakdown-bar-fill" style={{ width: "3%" }} />
-                </div>
-                <span>3%</span>
-              </div>
-              <div className="reviewlist-average-rating-breakdown-item">
-                <span>1</span>
-                {/* // TODO - THIS BAR STARTS SLIGHTLY BEFORE OTHERS */}
-                <div className="reviewlist-average-rating-breakdown-bar">
-                  <div className="reviewlist-average-rating-breakdown-bar-fill" style={{ width: "2%" }} />
-                </div>
-                <span>2%</span>
-              </div>
+            </div>
+            <div className="reviewlist-reviews">
+              {sortedAndFilteredReviews.map((review) => (
+                <Review key={review.id} review={review} />
+              ))}
+            </div>
+            <div className="reviewlist-showmore">
+              <p>
+                Show More <FontAwesomeIcon icon={faChevronDown} size="2xs" />
+              </p>
             </div>
           </div>
         </div>
-        <div className="reviewlist-right">
-          <div className="reviewlist-header">
-            <div className="reviewlist-placeholder">
-              <h2>Customer Reviews</h2>
-            </div>
-            <div className="reviewlist-sort">
-              <label className="reviewlist-sort-label">Sort by:</label>
-              <div className="reviewlist-sort-filter">
-                <div className="reviewlist-sort-filter-option">
-                  <select id="sortOption" value={sortOption} onChange={handleSortChange}>
-                    <option value="rating">Highest Ratings</option>
-                    <option value="lowestRating">Lowest Ratings</option> {/* Added option for lowest ratings */}
-                    <option value="date">Most Recent</option>
-                    <option value="helpful">Most Helpful</option>
-                  </select>
-                </div>
-              </div>
-              <div className="reviewlist-sort-verified">
-                <label className="reviewlist-sort-verified-label">
-                  Verified
-                  <input type="checkbox" checked={showVerifiedOnly} onChange={handleVerifiedChange} />
-                </label>
-              </div>
-            </div>
-          </div>
-          <div className="reviewlist-reviews">
-            {sortedAndFilteredReviews.map((review) => (
-              <Review key={review.id} review={review} />
-            ))}
-          </div>
-          <div className="reviewlist-showmore">
-            <p>
-              Show More <FontAwesomeIcon icon={faChevronDown} size="2xs" />
-            </p>
-          </div>
-        </div>
-      </div>
+      )}
     </div>
   );
 };
